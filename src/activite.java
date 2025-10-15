d6243ff47d5e9f066727c5f6cfde430e463251bf
Emeç Yıldız <emecyildiz@posta.mu.edu.tr>
2025-10-15 15:12:20 +0300

    çalışan uygulama gösterme eklendi

--- src/activite.java
+++ src/activite.java
@@ -1,2 +1,37 @@
+import java.io.*;
+
 public class activite {
+    public static void main(String[] args){
+        String os = System.getProperty("os.name").toLowerCase();
+        String line;
+        Process p;
+        if(os.contains("win")){
+            try {
+                p = Runtime.getRuntime().exec("tasklist.exe");
+                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                while((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+                input.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+        }else {
+            try {
+                p = Runtime.getRuntime().exec("ps -e");
+                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                while((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+                input.close();
+            }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+        }
+    }
 }
